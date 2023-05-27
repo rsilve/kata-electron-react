2f2414fc3226d6e31df80aa83015d4bc6b70779f
Robert Silve <robert@silve.net>
2023-05-27 13:24:58 +0200

    feat: format timestamp

--- src/renderer/src/components/Clock.tsx
+++ src/renderer/src/components/Clock.tsx
@@ -1,16 +1,22 @@
 import { Stack, Typography } from '@mui/material'
 import { useContext, useEffect, useState } from 'react'
 import { TimerContext } from '../context/TimerContextProvider'
+import dayjs from 'dayjs'
 
 const Clock = (): JSX.Element => {
   const context = useContext(TimerContext)
   const [time, setTime] = useState('')
-  useEffect(() => {
-    context.pull((value) => setTime(value))
-  })
+  useEffect(() =>
+    context.pull((value) => {
+      const formatted = dayjs(value).format('HH:mm:ss')
+      setTime(formatted)
+    })
+  )
   return (
     <Stack direction={'row'} justifyContent={'center'}>
-      <Typography variant={'h4'}>{time}</Typography>
+      <Typography variant={'h2'} fontFamily={'monospace'}>
+        {time}
+      </Typography>
     </Stack>
   )
 }
